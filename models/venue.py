5d17635a53df58be43180366f876df09d146aca0
macfluffy <shadowimitation@hotmail.com>
2025-10-14 18:01:10 +1100

    feat (Venue): Added the venue model

---
+++ models/venue.py
@@ -0,0 +1,21 @@
+"""
+This file defines the model for the 'venues' table and it's relationships. 
+"""
+# Local imports
+from init import db
+
+class Venue(db.Model):
+    """
+    The deck table template. This contains the name of a venue, their 
+    contact details, location and the relationships between the venue and
+    the events held at their location.
+    """
+
+    # Name of the table and what is referenced by Flask-SQLAlchemy methods
+    __tablename__ = "venues"
+
+    # Table columns
+    venue_id = db.Column(db.Integer, primary_key = True)
+    venue_name = db.Column(db.String(), nullable = False)
+    venue_address = db.Column(db.String())
+    venue_number = db.Column(db.String())
