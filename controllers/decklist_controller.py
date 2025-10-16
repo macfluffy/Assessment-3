eb5e8e25c31cc4cb44927c576e2c850ec071ad27
macfluffy <shadowimitation@hotmail.com>
2025-10-16 14:29:23 +1100

    feat (Decklist Controller): Corrected the blueprint to refer to the decklists junction table isntead of the cards table.

--- controllers/decklist_controller.py
+++ controllers/decklist_controller.py
@@ -13,7 +13,7 @@ from schemas.schemas import decklist_schema, decklists_schema
 
 # Create the Template Web Application Interface for card routes to be applied 
 # to the Flask application
-decklistsBp = Blueprint("cards", __name__, url_prefix = "/decklists")
+decklistsBp = Blueprint("decklists", __name__, url_prefix = "/decklists")
 
 
 """
@@ -38,7 +38,7 @@ API Routes
 def create_decklist():
     """
     Retrieve the body data and add the details of the decklist into the decklist database,
-    this is the equivalent of POST in postgresql.
+    this is the equivalent of POST in postgresql. This is how we add cards into a deck.
     """
     # Fetch the decklist information from the request body
     bodyData = request.get_json()
@@ -67,17 +67,17 @@ def get_decklists():
     Retrieve and read all the decklists from the decklists database,
     this is the equivalent of GET in postgresql.
     """
-    # Select all the decklists from the database and the students that
-    # are enrolled in these courses
+    # Select all the decklists from the database and the cards that
+    # are in these decks
     deck_id = request.args.get("deck_id", type = int)
     card_id = request.args.get("card_id", type = int)
     statement = db.select(Decklist)
     
-    # Display decklists that exist
+    # Display decks that exist
     if deck_id:
         statement = statement.where(Decklist.deck_id == deck_id)
     
-    # Display students that exist in the decklist
+    # Display cards that exist in these decks
     if card_id:
         statement = statement.where(Decklist.card_id == card_id)
 
@@ -95,15 +95,17 @@ def get_decklists():
         return error_empty_table()
     
 
-@decklistsBp.route("/<int:deck_id>&<int:card_id>", methods = ["DELETE"])
-def delete_decklist(deck_id, card_id):
+@decklistsBp.route("/deck_id=<int:deck_id>&card_id=<int:card_id>", methods = ["DELETE"])
+def delete_card_from_decklist(deck_id, card_id):
     """
-    Find the decklist with the matching ID in the decklist database and remove it.
-    This is the equivalent of DELETE in postgresql.
+    Find the card in the deck with the matching IDs and remove it from 
+    the decklist database. This is the equivalent of DELETE in 
+    postgresql.
     """
-    # Selects all the decklists from the database and filter the decklist with
-    # matching ID
+    # Selects all the decklists from the database and filter for the deck 
+    # with matching ID
     statement = db.select(Decklist).where(Decklist.deck_id == deck_id)
+    statement = db.select(Decklist).where(Decklist.card_id == card_id)
     
     # Serialise it as the scalar result is unserialised
     decklist = db.session.scalar(statement)
